31e82a671ea4d825c3e58d2b6ea18c36375fc5a9
Pedro Silva <pedro_rike@hotmail.com.br>
2023-09-07 00:30:24 -0300

    feat: new route

---
+++ src/controllers/Hello.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+export const HelloController = {
+  get: async (req: Request, res: Response) => {
+    try {
+      return res.status(200).json({message: "Hello world!"})
+
+    } catch (err: any) {
+      return res.status(500).json({
+        message: err.message || 'Internal Server Error',
+      })
+    }
+  },
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { upload } from '../config/multer'
 import { UploadImageController } from '../controllers/UploadImage'
+import { HelloController } from '../controllers/Hello'
 
 const routes = Router()
 
+routes.get('/', HelloController.get)
 routes.post('/upload-image', upload.single('file'), UploadImageController.upload)
 
 export default routes
