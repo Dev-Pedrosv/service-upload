c035d14ea417f92d6585cee77b61d67d84229d88
Pedro Silva <pedro_rike@hotmail.com.br>
2023-09-10 14:47:19 -0300

    feat: add validation type file

--- src/controllers/UploadImage.ts
+++ src/controllers/UploadImage.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from 'express'
 import { uploadFile } from '../config/upload'
 
+const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
 export const UploadImageController = {
   upload: async (req: Request, res: Response) => {
     try {
       if(!req.file){
         throw new Error('Missing file')
+      }
+
+      const fileExtension = req.file.originalname.split('.').pop().toLowerCase();
+
+      if (!imageExtensions.includes(fileExtension)) {
+        throw new Error('Invalid file type. Only image files are allowed.');
       }
 
       const imageUrl  = await uploadFile(req)
